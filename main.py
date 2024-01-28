18933d403fb6717e6289ee26d365456ca9006b1a
Fisch37 <seracin4@gmail.com>
2024-01-28 12:25:00 +0100

    Updated test script

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from nbt_monitor import NBTMonitor
 
 with NBTMonitor(("localhost", 25575), "very_secure_password") as monitor:
     monitor.wait()
-    for player, nbt in monitor.data.items():
-        print(player, "with", nbt)
+    for player, data in monitor.data.items():
+        print(data.model_dump_json(indent=4))
