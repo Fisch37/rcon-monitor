5681ffa608f73f8d450adc5732d8df324d6277b7
Fisch37 <seracin4@gmail.com>
2024-02-04 13:01:38 +0100

    Updated main.py example

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from nbt_monitor import NBTMonitor
 
 with NBTMonitor(("localhost", 25575), "very_secure_password") as monitor:
-    monitor.wait()
-    for player, data in monitor.data.items():
-        print(data.model_dump_json(indent=4))
+    while True:
+        monitor.wait()
+        for player, data in monitor.data.items():
+            with open(player + ".json", "w") as file:
+                file.truncate()
+                file.write(data.model_dump_json(indent=4))
