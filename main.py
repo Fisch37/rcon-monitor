93b20f55f172585bc9abdf4fbf0657c39ef300b0
Fisch37 <seracin4@gmail.com>
2024-02-04 13:47:45 +0100

    Implemented rudimentary api

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-from nbt_monitor import NBTMonitor
+from uvicorn import run
+from fastapi import FastAPI
 
-with NBTMonitor(("localhost", 25575), "very_secure_password") as monitor:
-    while True:
-        monitor.wait()
-        for player, data in monitor.data.items():
-            with open(player + ".json", "w") as file:
-                file.truncate()
-                file.write(data.model_dump_json(indent=4))
+from nbt_monitor import NBTMonitor, NBTInfo
+from api_models import *
+
+
+app = FastAPI()
+nbt_monitor: NBTMonitor
+
+
+@app.get("/players/names")
+def get_player_names() -> list[str]:
+    return list(nbt_monitor.data.keys())
+
+@app.get("/players")
+def get_all_player_data() -> dict[str, NBTInfo]:
+    return nbt_monitor.data
+
+@app.get("/players/location")
+def get_player_locations() -> dict[str, LocationInfo]:
+    return {
+        name: LocationInfo.from_nbtinfo(info)
+        for name, info in nbt_monitor.data.items()
+    }
+
+
+def main():
+    global nbt_monitor
+    nbt_monitor = NBTMonitor(("127.0.0.1", 25575), "very_secure_pswd")
+    
+    with nbt_monitor:
+        run(app)
+
+if __name__ == "__main__":
+    main()
