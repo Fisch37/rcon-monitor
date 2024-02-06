6a9ca26b76ec42ca09df512d407ff6eaea7c8f2e
Fisch37 <seracin4@gmail.com>
2024-02-06 22:25:27 +0100

    Added location and inventory HTTP API

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+from typing import Annotated
 from uvicorn import run
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 
 from nbt_monitor import NBTMonitor, NBTInfo
-from nbt_monitor.models import LocationInfo
+from nbt_monitor.models import LocationInfo, InventoryInfo
 
 
 app = FastAPI()
 nbt_monitor: NBTMonitor
+
+
+def player_data(name: str):
+    try:
+        return nbt_monitor.data[name]
+    except KeyError as e:
+        raise HTTPException(404, "Player not found") from e
+
+DependPlayerData = Annotated[NBTInfo, Depends(player_data)]
 
 
 @app.get("/players/names")
@@ -17,12 +27,31 @@ def get_player_names() -> list[str]:
 def get_all_player_data() -> dict[str, NBTInfo]:
     return nbt_monitor.data
 
+@app.get("/player/{name}")
+def get_player_data(info: DependPlayerData) -> NBTInfo:
+    return info
+
 @app.get("/players/location")
 def get_player_locations() -> dict[str, LocationInfo]:
     return {
         name: info.location
         for name, info in nbt_monitor.data.items()
     }
+
+@app.get("/players/{name}/location")
+def get_player_location(info: DependPlayerData) -> LocationInfo:
+    return info.location
+
+@app.get("/players/inventory")
+def get_player_inventories() -> dict[str, InventoryInfo]:
+    return {
+        name: info.inventory
+        for name, info in nbt_monitor.data.items()
+    }
+
+@app.get("/player/{name}/inventory")
+def get_player_inventory(info: DependPlayerData) -> InventoryInfo:
+    return info.inventory
 
 
 def main():
